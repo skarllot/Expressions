af502ec39aed678db340eed9e66a6fc289d7d07d
Fabrício Godoy <skarllot@gmail.com>
2023-02-25 18:26:04 -0300

    Create tests for QueryModel

---
+++ tests/Expressions.Tests/Queries/QueryModelTest.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+using Raiqub.Expressions.Queries;
+using Raiqub.Expressions.Tests.Examples;
+
+namespace Raiqub.Expressions.Tests.Queries;
+
+public class QueryModelTest
+{
+    [Fact]
+    public void CreateShouldAlwaysReturnAll()
+    {
+        var queryModel = QueryModel.Create<string?>();
+
+        string?[] result = new[] { "john", "jane", null, "hugo", "jack" }
+            .Apply(queryModel)
+            .ToArray();
+
+        result.Should().Equal("john", "jane", null, "hugo", "jack");
+    }
+
+    [Fact]
+    public void CreateShouldEvaluateSpecificationCorrectly()
+    {
+        var queryModel = QueryModel.Create(new StringBeginsWithJohnSpecification());
+
+        string[] result = new[] { "john", "jane", "hugo", "jack" }
+            .Apply(queryModel)
+            .ToArray();
+
+        result.Should().Equal("john");
+    }
+}
