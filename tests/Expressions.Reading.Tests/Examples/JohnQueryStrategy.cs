54ac660febabe49e1bc748a985b47b4dea08c021
Fabrício Godoy <skarllot@users.noreply.github.com>
2024-01-07 15:33:23 -0300

    test(read): increase coverage of reading library (#92)

---
+++ tests/Expressions.Reading.Tests/Examples/JohnQueryStrategy.cs
@@ -0,0 +1,21 @@
+﻿using Raiqub.Expressions.Queries;
+
+namespace Raiqub.Expressions.Reading.Tests.Examples;
+
+public class JohnQueryStrategy : EntityQueryStrategy<string>
+{
+    public JohnQueryStrategy(params Specification<string>[] restrictions)
+        : base(restrictions)
+    {
+    }
+
+    public JohnQueryStrategy(IEnumerable<Specification<string>> restrictions)
+        : base(restrictions)
+    {
+    }
+
+    protected override IQueryable<string> ExecuteCore(IQueryable<string> source)
+    {
+        return source.Where(x => x.StartsWith("john", StringComparison.InvariantCultureIgnoreCase));
+    }
+}
---
+++ tests/Expressions.Reading.Tests/Examples/NewsPostThatContainsFirstQueryStrategy.cs
@@ -0,0 +1,9 @@
+﻿using Raiqub.Expressions.Queries;
+
+namespace Raiqub.Expressions.Reading.Tests.Examples;
+
+public class NewsPostThatContainsFirstQueryStrategy : EntityQueryStrategy<NewsPost, BlogPost>
+{
+    protected override IQueryable<BlogPost> ExecuteCore(IQueryable<NewsPost> source) => source.Where(
+        p => p.Content.Contains("first", StringComparison.InvariantCultureIgnoreCase));
+}
